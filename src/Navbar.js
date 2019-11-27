b8111167849b791f9706ab68b7a4cd748c93861f
Liviu Preda <liviupreda@gmail.com>
2019-11-27 15:30:19 +0200

    style active page in the navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -6,10 +6,18 @@ class Navbar extends Component {
   render() {
     return (
       <div className="Navbar">
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/chips">Chips</NavLink>
-        <NavLink to="/soda">Soda</NavLink>
-        <NavLink to="/sardines">Sardines</NavLink>
+        <NavLink exact activeClassName="Navbar-active" to="/">
+          Home
+        </NavLink>
+        <NavLink exact activeClassName="Navbar-active" to="/chips">
+          Chips
+        </NavLink>
+        <NavLink exact activeClassName="Navbar-active" to="/soda">
+          Soda
+        </NavLink>
+        <NavLink exact activeClassName="Navbar-active" to="/sardines">
+          Sardines
+        </NavLink>
       </div>
     );
   }
